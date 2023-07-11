4bcfdd2a45a37f5f58ca7861d5f38792b0fcc8a4
MerajCodes <113118915+merajCodes3440@users.noreply.github.com>
2023-07-11 11:26:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function updateTextAfterDelay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Example usage
+const outputElement = document.getElementById("output");
+
+updateTextAfterDelay()
+  .then((text) => {
+    outputElement.textContent = text;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
